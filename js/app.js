38417365229a5b7ee1dbb7c8cf30e91337b0462c
seokenny <kenn.seo94@gmail.com>
2019-10-21 15:22:47 -0400

    Added mobile navigation animation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+var counter = 0;
+
 function onMenuClick() {
     document.getElementById("m_nav").classList.toggle("change");
+    $("#menu-bg").toggleClass("change-bg");
+    if(counter === 0){
+        document.getElementById("mobile_navigation").classList.toggle("hidden");
+        counter++;
+    }
+    document.getElementById("mobile_navigation").classList.toggle("slideOutUp");
+    document.getElementById("mobile_navigation").classList.toggle("slideInDown");
+}
+
+function onNavClick() {
+    var navList = [];
+    navList = document.getElementsByClassName("m_nav_li");
+    for(var i = 0; i < navList.length; i++){
+        navList[i].classList.toggle("left_handed_css");
+    }
 }
